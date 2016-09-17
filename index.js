5ed102b7695ce0a74b59ab755a864e8a6e33f33c
aleczander <afigueroa81@ufl.edu>
2016-09-16 20:58:12 -0400

    new html files

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ server.get('/', function (request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 
+server.get('/signup', function (request, response){
+response.sendFile('public/html/signup.html', {root:__dirname});
+
+});
+
+server.get('/login', function (request, response){
+  response.sendFile('public/html/login.html', {root:__dirname});
+});
+
+
 server.listen(8080, function(){
   console.log("now listnening on port...", 8080);
 });
